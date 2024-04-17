84f33322e07201e5606ba2fc0fc318cddca9e65d
Bello-Ibrahm <ibrahimbello1243@gmail.com>
2024-04-17 06:07:46 -0700

    Fix checker requirement #4

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -13,18 +13,6 @@ def canUnlockAll(boxes):
 
     Returns:
     bool: True if all boxes can be unlocked, otherwise False.
-
-    The function follows a breadth-first search approach using a queue
-    to keep track of the boxes that have been unlocked.
-    Initially, the function starts with the first box (box 0),
-    which is considered unlocked. The function then processes the
-    queue, iterating through each key in the current box and unlocking
-    the corresponding box if it has not been opened yet. It continues
-    until there are no more boxes left to process in the queue.
-
-    If all boxes are opened (i.e., the length of the set of opened boxes
-    is equal to the total number of boxes), the function
-    returns True. Otherwise, it returns False.
     """
     n = len(boxes)
     seen_boxes = set([0])
